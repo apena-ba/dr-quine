4cc3a6fbbe7f639064d5162031532bd488b10902
Adri <adripbar@gmail.com>
2023-05-05 22:44:22 +0200

    Finish fix Grace

--- C_QUINE/Grace.c
+++ C_QUINE/Grace.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10, 10);}%c%c// Comment%c%cFT(STR)%c"
-#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10, 10);}
+#define OPEN(file) open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c#define OPEN(file) open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)%c#define STR %c%s%c%c#define FT(x) int main(){int file = OPEN(%cGrace_kid.c%c); dprintf(file, x, 10, 10, 10, 10, 10, 34, x, 34, 10, 34, 34, 10, 10, 10, 10, 10); close(file);}%c%c// Comment%c%cFT(STR)%c"
+#define FT(x) int main(){int file = OPEN("Grace_kid.c"); dprintf(file, x, 10, 10, 10, 10, 10, 34, x, 34, 10, 34, 34, 10, 10, 10, 10, 10); close(file);}
 
 // Comment
 
