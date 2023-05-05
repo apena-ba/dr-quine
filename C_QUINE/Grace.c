c7c9b0191d7e412f0559f32eefd71f3feff247ad
Adri <adripbar@gmail.com>
2023-05-05 21:48:48 +0200

    Add Grace done

--- C_QUINE/Grace.c
+++ C_QUINE/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define STR "Pepe"
-#define FT(x) int main(){printf(x);}
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10);}%c%cFT(STR)%c"
+#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10);}
 
 FT(STR)
