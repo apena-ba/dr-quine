ac17d3e5205b978c6b340ff561fc9f27e0cf434f
Adri <adripbar@gmail.com>
2023-05-05 22:16:45 +0200

    Add comment to Grace

--- C_QUINE/Grace.c
+++ C_QUINE/Grace.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10);}%c%cFT(STR)%c"
-#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10);}
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10, 10);}%c%c// Comment%c%cFT(STR)%c"
+#define FT(x) int main(){printf(x, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10, 10);}
+
+// Comment
 
 FT(STR)
