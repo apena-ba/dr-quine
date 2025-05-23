25db782d8709a307683f34f1a1436da525d23e59
Adrian Pena barragan <apena-ba@c3r8s3.42malaga.com>
2025-05-24 00:44:37 +0200

    Finish Grace ASM and fix Makefile

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,13 +1,13 @@
 ; This program will print its own source when run.
 
 section .data
-code: db "; This program will print its own source when run.%1$c%1$csection .data%1$ccode: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$cextern fflush%1$cmain:%1$c%4$c; Main comment%1$c%4$cand rsp, -16%1$c%4$cmov rdi, code%1$c%4$cmov rsi, 10%1$c%4$cmov rdx, 34%1$c%4$cmov rcx, code%1$c%4$cmov r8, 9%1$c%4$cxor rax, rax%1$c%1$c%4$ccall printf%1$c%1$c%4$cmov rdi, 0%1$c%4$ccall fflush%1$c%1$c%4$cmov rax, 60%1$c%4$cxor rdi, rdi%1$c%4$csyscall%1$c", 0
+code: db "; This program will print its own source when run.%1$c%1$csection .data%1$ccode: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal _start%1$cextern printf%1$cextern fflush%1$c_start:%1$c%4$c; Main comment%1$c%4$cand rsp, -16%1$c%4$cmov rdi, code%1$c%4$cmov rsi, 10%1$c%4$cmov rdx, 34%1$c%4$cmov rcx, code%1$c%4$cmov r8, 9%1$c%4$cxor rax, rax%1$c%1$c%4$ccall printf%1$c%1$c%4$cmov rdi, 0%1$c%4$ccall fflush%1$c%1$c%4$cmov rax, 60%1$c%4$cxor rdi, rdi%1$c%4$csyscall%1$c", 0
 
 section .text
-global main
+global _start
 extern printf
 extern fflush
-main:
+_start:
 	; Main comment
 	and rsp, -16
 	mov rdi, code
